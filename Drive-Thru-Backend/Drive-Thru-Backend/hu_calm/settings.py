f13161149b0f5f1d1d651f04f4b654b9f0272da9
hruthika <hruthika.sa258@gmail.com>
2024-08-09 21:55:53 +0530

    changed settinds.py

--- Drive-Thru-Backend/Drive-Thru-Backend/hu_calm/settings.py
+++ Drive-Thru-Backend/Drive-Thru-Backend/hu_calm/settings.py
@@ -70,15 +70,12 @@ WSGI_APPLICATION = 'hu_calm.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
-            'USER': 'postgres',
-            'PASSWORD': 'Faiza123',
-            'HOST': 'drivethru2.cqlgsm6royql.us-east-1.rds.amazonaws.com',
-            'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
